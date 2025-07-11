17a180399d9098c1aa862e07a49c8bd21fa30c15
Enrique Lacambra III <elacambra@usasean.org>
2025-07-11 15:20:44 +0800

    Update Serverless

--- utils/fields/index.js
+++ utils/fields/index.js
@@ -382,6 +382,36 @@ const generateObject = (req, temp ,callback) =>{
 		// Object.assign(obj, {"related-projects": false})
 
 	}
+	else if(temp.params.collectionid == process.env.WEBFLOW_20){
+		let answers = req.form_response.answers
+		let body = {
+			client: answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0] ? answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0].text : null,
+			name: answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0] ? answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0].text : null,
+			['view-live-website']: answers.filter((item)=> item.field.ref === 'ff2cbb49-aca0-4856-a8fb-3b7b66aa17d9')[0] ? answers.filter((item)=> item.field.ref === 'ff2cbb49-aca0-4856-a8fb-3b7b66aa17d9')[0].url : null,
+			['company-email']: answers.filter((item)=> item.field.ref === 'a90973cf-92b6-46fc-8ee8-cd8d4f1a1efa')[0] ? answers.filter((item)=> item.field.ref === 'a90973cf-92b6-46fc-8ee8-cd8d4f1a1efa')[0].email : null,
+			['csr-report']: answers.filter((item)=> item.field.ref === 'd7aec445-86a8-472b-ae1e-4cbc700c1add')[0] ? answers.filter((item)=> item.field.ref === 'd7aec445-86a8-472b-ae1e-4cbc700c1add')[0].file_url : null,
+			['main-project-image']: answers.filter((item)=> item.field.ref === '554ab0e2-6af7-4942-bf01-f392fe27fbac')[0] ? answers.filter((item)=> item.field.ref === '554ab0e2-6af7-4942-bf01-f392fe27fbac')[0].file_url : null,
+			['project-details']: answers.filter((item)=> item.field.ref === 'dd88ec3e-224e-4c04-b87a-66a893b38478')[0] ? answers.filter((item)=> item.field.ref === 'dd88ec3e-224e-4c04-b87a-66a893b38478')[0].text : null,
+			['key-highlights']: answers.filter((item)=> item.field.ref === '91ca2dc5-95f0-4b05-bf1e-b4c1c2207ac8')[0] ? answers.filter((item)=> item.field.ref === '91ca2dc5-95f0-4b05-bf1e-b4c1c2207ac8')[0].text : null,
+			['quick-facts-2']: answers.filter((item)=> item.field.ref === '8c0b232e-59a9-451f-a35f-af81580e9dee')[0] ? answers.filter((item)=> item.field.ref === '8c0b232e-59a9-451f-a35f-af81580e9dee')[0].text : null,
+			['human-capital-development-2']: answers.filter((item)=> item.field.ref === '145ccfc9-15e8-45f3-ae4d-fd2240672c8a')[0] ? answers.filter((item)=> item.field.ref === '145ccfc9-15e8-45f3-ae4d-fd2240672c8a')[0].text : null,
+			['social-and-community-support-2']: answers.filter((item)=> item.field.ref === '219a67d0-95b8-428d-b32f-08e397450ecc')[0] ? answers.filter((item)=> item.field.ref === '219a67d0-95b8-428d-b32f-08e397450ecc')[0].text : null,
+			['sme-support-2']: answers.filter((item)=> item.field.ref === 'fdf6fc23-adbc-4d70-a5be-ce8215712125')[0] ? answers.filter((item)=> item.field.ref === 'fdf6fc23-adbc-4d70-a5be-ce8215712125')[0].text : null,
+			['covid19-support']: answers.filter((item)=> item.field.ref === 'c10e1c79-640a-4d89-a0d5-8e0376d1c902')[0] ? answers.filter((item)=> item.field.ref === 'c10e1c79-640a-4d89-a0d5-8e0376d1c902')[0].text : null,
+			['other-2']: answers.filter((item)=> item.field.ref === '3bbf39b9-1b10-4001-8a88-b34d265d9e25')[0] ? answers.filter((item)=> item.field.ref === '3bbf39b9-1b10-4001-8a88-b34d265d9e25')[0].text : null
+		}
+		console.log(body)
+		Object.assign(obj, body)
+		console.log(`[Singapore Microsite 2025] Adding a new record to a collection with an ID of: ${process.env.WEBFLOW_20}`)
+		// Object.assign(obj, {'client': req.form_response.answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0].text})
+		// Object.assign(obj, {'name': req.form_response.answers.filter((item)=> item.field.ref === '01F1HK19XVNTGAKF4V80FF07D3')[0].text})
+		// Object.assign(obj, {'view-live-website': req.form_response.answers.filter((item)=> item.field.ref === 'ff2cbb49-aca0-4856-a8fb-3b7b66aa17d9')[0].url})
+		// Object.assign(obj, {'company-email':req.form_response.answers.filter((item)=> item.field.ref === 'a90973cf-92b6-46fc-8ee8-cd8d4f1a1efa')[0].email})
+		// Object.assign(obj, {'csr-report':{'url': req.form_response.answers.filter((item)=> item.field.ref === 'd7aec445-86a8-472b-ae1e-4cbc700c1add')[0].file_url}})
+		// Object.assign(obj, {'main-project-image':{'url':req.form_response.answers.filter((item)=> item.field.ref === '554ab0e2-6af7-4942-bf01-f392fe27fbac')[0].file_url}})
+		// Object.assign(obj, {'project-details':req.form_response.answers.filter((item)=> item.field.ref === 'dd88ec3e-224e-4c04-b87a-66a893b38478')[0].text})
+		// Object.assign(obj, {"related-projects": false})
+	}
 	//Add another if statement for every new typeform fields
 	//Add collectionid value to .env for the if statement
 	//else if(req.params.collectionid == process.env.WEBFLOW_#){...WEBFLOW FIELDS...}test
